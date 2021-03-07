a05b0c4419b425d4e5fd3a5d0dffcc7d6e731ae1
최윤정 <jjjung1003@naver.com>
2021-03-07 09:58:34 +0900

    onCreate / onResume 실행됨

--- app/src/main/java/com/thejoeun/lifecycle_20210307/MainActivity.kt
+++ app/src/main/java/com/thejoeun/lifecycle_20210307/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.thejoeun.lifecycle_20210307
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,8 +11,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.d("메인화면", "onCreate 실행됨")
         moveToOtherBtn.setOnClickListener {
 
+            val myIntent = Intent(this, OtherActivity::class.java)
+
+            startActivity(myIntent)
+
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        
+        Log.d("메인화면", "리줌이 실행")
     }
 }
--- app/src/main/java/com/thejoeun/lifecycle_20210307/OtherActivity.kt
+++ app/src/main/java/com/thejoeun/lifecycle_20210307/OtherActivity.kt
@@ -2,10 +2,14 @@ package com.thejoeun.lifecycle_20210307
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        Log.d("다른화면", "onCreate 실행됨")
+
     }
 }
